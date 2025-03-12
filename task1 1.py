4c02c22bf110e06b28736c2d1c706efe9784b7cf
Mrunal Buchkule <mrunalbuchkule@gmail.com>
2025-03-12 21:41:58 +0530

    task1

---
+++ task1 1.py
@@ -0,0 +1,8 @@
+full_name = input("Enter your full name: ")
+
+title_case_name = full_name.title()
+
+char_count = len(full_name.replace(" ", ""))
+
+print("Name in title case:", title_case_name)
+print("Total number of characters (excluding spaces):", char_count)
